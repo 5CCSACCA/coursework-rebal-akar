111897b3d2116ca617d1246754d5f5647f3c3aa5
Rebal Akar <rebal.akar@kcl.ac.uk>
2024-11-27 17:09:54 +0000

    chore: initialize FastAPI project structure

---
+++ app/auth_service/main.py
@@ -0,0 +1,26 @@
+# auth_service/main.py
+from fastapi import FastAPI
+from routers import users
+from database.mongodb import connect_to_mongo, close_mongo_connection
+from fastapi.middleware.cors import CORSMiddleware  
+app = FastAPI(
+    title="Authentication Service",
+    description="Handles user registration and authentication.",
+    version="1.0.0",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8001","http://127.0.0.1:8001"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
+app.include_router(users.router, tags=["Users"], prefix="/users")
