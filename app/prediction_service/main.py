ce887f7f442a3584ef7cadaafe8b025a92f55a12
Rebal Akar <rebal.akar@kcl.ac.uk>
2024-11-27 19:25:21 +0000

    chore(req_service): initialize prediction service project structure

---
+++ app/prediction_service/main.py
@@ -0,0 +1,16 @@
+# prediction_service/main.py
+from fastapi import FastAPI
+from routers import predict
+from database.mongodb import connect_to_mongo, close_mongo_connection
+
+
+app = FastAPI(
+    title="Prediction Service",
+    description="Handles predictions using the machine learning model.",
+    version="1.0.0",
+)
+
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
+app.include_router(predict.router, tags=["Predictions"], prefix="/predict")
