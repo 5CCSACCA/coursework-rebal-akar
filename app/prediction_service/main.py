503f35d95ff10a1b4c4e991c9936d479e572f825
Rebal Akar <rebal.akar@kcl.ac.uk>
2024-11-27 19:32:18 +0000

    chore(main): configure main with routers, CORS and MongoDB event handlers

--- app/prediction_service/main.py
+++ app/prediction_service/main.py
@@ -1,8 +1,10 @@
 # prediction_service/main.py
+
 from fastapi import FastAPI
 from routers import predict
 from database.mongodb import connect_to_mongo, close_mongo_connection
-
+from routers import users, health
+from routers import users, predict, health
 
 app = FastAPI(
     title="Prediction Service",
@@ -13,4 +15,7 @@ app = FastAPI(
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)
 
+app.include_router(users.router, tags=["Users"], prefix="/users")
 app.include_router(predict.router, tags=["Predictions"], prefix="/predict")
+app.include_router(health.router, tags=["Health"])
+
