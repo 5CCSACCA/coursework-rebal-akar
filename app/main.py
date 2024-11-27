f344698bfd9835a8531fa1ac1208c094451072f1
Rebal Akar <rebal.akar@kcl.ac.uk>
2024-11-27 19:43:35 +0000

    fix(pred_service): add missing __init__

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from routers import users, predict
-
-app = FastAPI()
-
-app.include_router(users.router, prefix="/users", tags=["users"])
-app.include_router(predict.router, tags=["predict"])
